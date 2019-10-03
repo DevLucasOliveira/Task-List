7410779d1ee9df4026ac9c5151e5032e2c663359
Lucas Oliveira <lucasoliveira.si@outlook.com>
2019-10-03 17:12:05 -0300

    creating variables and showing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
+  
+ 
+  constructor() {
+    this.todos.push('passear com o cachorro');
+    this.todos.push('ir ao supermercado');
+    this.todos.push('cortar o cabelo')
+  }
+
 }
